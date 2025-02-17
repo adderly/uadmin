257996832bf54b6bfde178c20161b37524c452eb
adderly <adderlygonzalez@gmail.com>
2025-02-17 08:13:26 -0400

    * Added new models

--- internal/captcha/captcha_handler.go
+++ internal/captcha/captcha_handler.go
@@ -1,9 +1,12 @@
 package captcha
 
 import (
+	"github.com/kataras/iris/v12"
 	"github.com/uadmin/uadmin/internal/captcha/logic/captdata"
 	"github.com/uadmin/uadmin/internal/captcha/logic/checkdata"
+	"log"
 	"net/http"
+	"time"
 )
 
 func CaptchaHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +17,42 @@ func CaptchaHandler(w http.ResponseWriter, r *http.Request) {
 		checkdata.CheckClickData(w, r)
 		return
 	}
+}
+
+// Create middleware for requiring request with captcha code
+
+// Function that receive all requests and handle the type of captcha accordingly.
+func UadminIrisAuthMidleware(ctx iris.Context) {
+	t := time.Now()
+
+	// Set a shared variable between handlers
+	ctx.Values().Set("framework", "iris")
+
+	// before request
+
+	//req := ctx.Request()
+
+	//s := uadmin.IsAuthenticated(req)
+	//log.Println(s)
+	//if s == nil {
+	//	ctx.Values().Set("message", "this is the error message")
+	//	ctx.StatusCode(iris.StatusUnauthorized)
+	//	return
+	//} else {
+	//	SetContextSession(ctx, s)
+	//}
+
+	ctx.Next()
+
+	// after request
+	latency := time.Since(t)
+	log.Print(latency)
+
+	// access the status we are sending
+	status := ctx.GetStatusCode()
+	log.Println(status)
+}
+
+func ValidateCaptchaData(req *http.Request) error {
+	return nil
 }
--- internal/security/request_security_dto.go
+++ internal/security/request_security_dto.go
@@ -10,7 +10,26 @@ type RequestProofInputDto struct {
 }
 
 type RequestProofOutSlideDto struct {
-	code  string `json:"code,omitempty" form:"code,omitempty"`
+	code   int    `json:"code,omitempty" form:"code,omitempty"`
+	key    string `json:"key,omitempty" form:"key,omitempty"`
+	image  string `json:"image,omitempty" form:"image,omitempty"`
+	tile   string `json:"tile,omitempty" form:"tile,omitempty"`
+	tile_w int    `json:"tile_w,omitempty" form:"tile_w,omitempty"`
+	tile_h int    `json:"tile_h,omitempty" form:"tile_h,omitempty"`
+	tile_x int    `json:"tile_x,omitempty" form:"tile_x,omitempty"`
+	tile_y int    `json:"tile_y,omitempty" form:"tile_y,omitempty"`
+}
+
+type RequestProofOutRotateDto struct {
+	code  int    `json:"code,omitempty" form:"code,omitempty"`
 	key   string `json:"key,omitempty" form:"key,omitempty"`
 	image string `json:"image,omitempty" form:"image,omitempty"`
+	thumb string `json:"thumb,omitempty" form:"thumb,omitempty"`
+}
+
+type RequestProofOutClickDto struct {
+	code  int    `json:"code,omitempty" form:"code,omitempty"`
+	key   string `json:"key,omitempty" form:"key,omitempty"`
+	image string `json:"image,omitempty" form:"image,omitempty"`
+	thumb string `json:"thumb,omitempty" form:"thumb,omitempty"`
 }
